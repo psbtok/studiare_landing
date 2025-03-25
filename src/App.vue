b87b449d8f8c9f349f49684108b705e99e19fb10
Pete <opletaevpetr@mail.ru>
2025-03-25 18:51:01 +0300

    Initializing The Project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 10px;
+  padding: 10px;
+  background: #f3f3f3;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <p>Добро пожаловать на главную страницу!</p>
+    </div>
+</template>
+  
